c2cc7faf4fbf7a17a940a48f112925d4c4235973
Fernando Fernandez Monterroso <fernando.fernandezmo@plexus.tech>
2025-07-22 09:18:58 +0200

    Segundo commit

---
+++ src/main/java/com/pruebaA_JS/demo/PruebaAJsApplication.java
@@ -0,0 +1,13 @@
+package com.pruebaA_JS.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaAJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaAJsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebaA_JS/demo/PruebaAJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaA_JS.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaAJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
