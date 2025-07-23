16a15a0c382bf8028857aea3ceb280f9b67c2afd
daniel.canosevilla <danie.canosevilla@plexus.tech>
2025-07-23 11:47:16 +0200

    Post Funcional

--- back/src/main/java/com/pruebaA_JS/demo/services/UsersService.java
+++ back/src/main/java/com/pruebaA_JS/demo/services/UsersService.java
@@ -2,16 +2,14 @@ package com.pruebaA_JS.demo.services;
 
 import com.pruebaA_JS.demo.entities.Users;
 import com.pruebaA_JS.demo.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersService {
+    @Autowired
+    private  UsersRepository usersRepository;
 
-    private final UsersRepository usersRepository;
-
-    public UsersService(UsersRepository usersRepository) {
-        this.usersRepository = usersRepository;
-    }
 
     public void addUser(Users user){
         usersRepository.save(user);
