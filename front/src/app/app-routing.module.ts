df8b742f75972fe5a3571aaf83a819416ac0a56a
Pablo San Jose Diaz <pablo.sanjosediaz@plexus.tech>
2025-07-24 13:00:04 +0200

    Primer front

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LateralComponent } from './lateral/lateral.component';
+import { JornadaComponent } from './jornada/jornada.component';
+import { HorarioComponent } from './horario/horario.component';
+import { FichajeComponent } from './fichaje/fichaje.component';
 
 const routes: Routes = [
-  {path: '/', component: HomeComponent},
+  {path: '/login', component:LoginComponent},
+  {path: '/lateral', component:LateralComponent},
+  {path: '/jornada', component:JornadaComponent},
+  {path: '/horario', component:HorarioComponent},
+  {path: '/fichaje', component:FichajeComponent},
+  {path: '', pathMatch:'full', redirectTo:'login'}
 ];
 
 @NgModule({
