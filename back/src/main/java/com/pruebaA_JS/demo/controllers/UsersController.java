46805f2e0d13e95d1d89d7e4000bf4773ccd2fbf
Pablo San Jose Diaz <pablo.sanjosediaz@plexus.tech>
2025-07-31 14:13:56 +0200

    Funciona el JWT

--- back/src/main/java/com/pruebaA_JS/demo/controllers/UsersController.java
+++ back/src/main/java/com/pruebaA_JS/demo/controllers/UsersController.java
@@ -1,10 +1,19 @@
 package com.pruebaA_JS.demo.controllers;
 
+import com.pruebaA_JS.demo.entities.AuthRequest;
+import com.pruebaA_JS.demo.entities.AuthResponse;
 import com.pruebaA_JS.demo.entities.Users;
+import com.pruebaA_JS.demo.services.MyUserDetailsService;
 import com.pruebaA_JS.demo.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -16,6 +25,16 @@ public class UsersController {
 
     @Autowired
     private UsersService usersService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
 
     @PostMapping("/new")
     public Users registerUser(@RequestBody Users user) {
@@ -49,5 +68,23 @@ public class UsersController {
         System.out.println(email);
         return usersService.getUserWithEmail(email);
     }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequest authRequest) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales incorrectas");
+        }
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.getEmail());
+        final String jwt = jwtUtil.generateToken(userDetails.getUsername());
+
+        return ResponseEntity.ok(new AuthResponse(jwt));
+    }
+
 
 }
---
+++ back/src/main/java/com/pruebaA_JS/demo/entities/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.pruebaA_JS.demo.entities;
+
+public class AuthResponse {
+    private String token;
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
+
